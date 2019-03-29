747da2ef6aee29ea07b508439184eb7f87fc246f
Pavan Kumar C <pavankumar@railsfactory.org>
2019-03-29 18:57:59 +0530

    firebase package added and firebase configuration done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
+
 import { Header } from './components/common' 
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCkLQJhCKwizqT5nJ64-FQ9hgBvDRFuKgg',
+      authDomain: 'react-auth-a00ca.firebaseapp.com',
+      databaseURL: 'https://react-auth-a00ca.firebaseio.com',
+      projectId: 'react-auth-a00ca',
+      storageBucket: 'react-auth-a00ca.appspot.com',
+      messagingSenderId: '323150073539'
+    });
+  }
+
   render() {
     return (
       <View>
